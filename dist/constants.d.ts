b25c7335d14352c0ee468cd436710172f944dee5
tiamo <vk.tiamo@gmail.com>
2022-02-04 18:14:16 +0200

    clean main branch

--- dist/constants.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { Buffer } from 'buffer';
-import { PublicKey } from '@solana/web3.js';
-export declare const STAKE_POOL_PROGRAM_ID: PublicKey;
-export declare const MAX_VALIDATORS_TO_UPDATE = 5;
-export declare const TRANSIENT_STAKE_SEED_PREFIX: Buffer;
-export declare const MIN_STAKE_BALANCE: number;
--- dist/utils/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './math';
-export * from './program-address';
-export * from './stake';
-export * from './token';
-export declare function arrayChunk(array: any[], size: number): any[];
