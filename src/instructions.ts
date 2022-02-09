35e884f6c6411e8addafa58cae547c9a97307ff3
tiamo <vk.tiamo@gmail.com>
2022-02-09 17:50:07 +0200

    chore: refactory

--- src/instructions.ts
+++ src/instructions.ts
@@ -11,51 +11,7 @@ import {
 import * as BufferLayout from '@solana/buffer-layout';
 import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import { STAKE_POOL_PROGRAM_ID } from './constants';
-import { Buffer } from 'buffer';
-
-/**
- * @internal
- */
-export type InstructionType = {
-  /** The Instruction index (from solana upstream program) */
-  index: number;
-  /** The BufferLayout to use to build data */
-  layout: BufferLayout.Layout<any>;
-};
-
-/**
- * Populate a buffer of instruction data using an InstructionType
- * @internal
- */
-export function encodeData(type: InstructionType, fields?: any): Buffer {
-  const allocLength = type.layout.span;
-  const data = Buffer.alloc(allocLength);
-  const layoutFields = Object.assign({ instruction: type.index }, fields);
-  type.layout.encode(layoutFields, data);
-
-  return data;
-}
-
-/**
- * Decode instruction data buffer using an InstructionType
- * @internal
- */
-export function decodeData(type: InstructionType, buffer: Buffer): any {
-  let data;
-  try {
-    data = type.layout.decode(buffer);
-  } catch (err) {
-    throw new Error('invalid instruction; ' + err);
-  }
-
-  if (data.instruction !== type.index) {
-    throw new Error(
-      `invalid instruction; instruction index mismatch ${data.instruction} != ${type.index}`,
-    );
-  }
-
-  return data;
-}
+import { InstructionType, encodeData, decodeData } from './utils';
 
 /**
  * An enumeration of valid StakePoolInstructionType's
