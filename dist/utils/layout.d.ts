543fb4357179affed5bffc50319812681c5a4adf
tiamo <vk.tiamo@gmail.com>
2023-03-14 16:51:36 +0200

    chore: cleanup

--- dist/utils/layout.d.ts
+++
@@ -1,9 +0,0 @@
-import * as BufferLayout from '@solana/buffer-layout';
-export declare class OptionLayout<T> extends BufferLayout.Layout<T | undefined> {
-    private readonly layout;
-    private readonly discriminator;
-    private constructor();
-    static of<T>(layout: BufferLayout.Layout<T>): OptionLayout<T>;
-    encode(src: T, b: Uint8Array, offset?: number): number;
-    decode(b: Uint8Array, offset?: number): T | undefined;
-}
